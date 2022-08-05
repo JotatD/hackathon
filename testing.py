4ba17b130c20fb0999b4f0bc7dfac5ca8fe8dca6
josetomas532 <jose.tomas.glhm@gmail.com>
2022-08-05 19:29:15 -0400

    Creating say_things function

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
-print(f"El chocolate es una cosa pero barbara")
+
+def say_things():
+    print(f"El chocolate es una cosa pero barbara")
+    print(f"Oye tranquilo viejo")
+
+
+if __name__ == "__main__":
+    say_things()
