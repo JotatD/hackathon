718b9b6bba21596eb4b9c7cdbd19c98086247367
josetomas532 <jose.tomas.glhm@gmail.com>
2022-08-20 21:14:47 -0400

    Adding GMV Optimization

--- endpoint.py
+++ endpoint.py
@@ -1,36 +1,12 @@
-from utils import get_url, process_date, get_header, process_data
-from financial import calculate_return, calculate_volatility
-import requests
+from utils import process_date
+from financial import *
 import pandas as pd
 
 
-def financial_metrics_calculation(coin_a, coin_b, time_unit, time_measure, initial_time, final_time):
+def financial_metrics_calculation(info_coin_a, info_coin_b, initial_time, final_time):
 
     start_time = process_date(initial_time)
     end_time = process_date(final_time)
-
-    coin_a_url = get_url(coin_a, time_unit, time_measure, initial_time, final_time)
-    coin_b_url = get_url(coin_b, time_unit, time_measure, initial_time, final_time)
-    header = get_header()
-
-    info_coin_a = requests.get(coin_a_url, headers=header).json()
-    
-    if 'error' in info_coin_a:
-        raise Exception(info_coin_a['error'])
-    elif len(info_coin_a) == 0:
-        raise Exception('Non-existing coin')
-
-    info_coin_a = process_data(info_coin_a)
-
-    info_coin_b = requests.get(coin_b_url, headers=header).json()
-
-    if 'error' in info_coin_b:
-        raise Exception(info_coin_b['error'])
-    elif len(info_coin_b) == 0:
-        raise Exception('Non-existing coin')
-
-    info_coin_b = process_data(info_coin_b)
-    
 
     info_coin_a = calculate_return(info_coin_a)
     info_coin_a = calculate_volatility(info_coin_a)
@@ -41,11 +17,41 @@ def financial_metrics_calculation(coin_a, coin_b, time_unit, time_measure, initi
     info_coin_b = info_coin_b.loc[start_time:end_time]
 
     returns_df = pd.DataFrame([])
-    returns_df[coin_a] = info_coin_a["returns"]
-    returns_df[coin_b] = info_coin_b["returns"]
+    returns_df[info_coin_a.columns.name] = info_coin_a["returns"]
+    returns_df[info_coin_b.columns.name] = info_coin_b["returns"]
 
     volatility_df = pd.DataFrame([])
-    volatility_df[coin_a] = info_coin_a["volatility"]
-    volatility_df[coin_b] = info_coin_b["volatility"]
+    volatility_df[info_coin_a.columns.name] = info_coin_a["volatility"]
+    volatility_df[info_coin_b.columns.name] = info_coin_b["volatility"]
 
     return returns_df, volatility_df
+
+
+def financial_data_comparison(returns_df):
+    complete_return = calculate_complete_return(returns_df)
+    complete_volatility = calculate_complete_volatility(returns_df)
+    comparison_data = pd.concat([complete_return, complete_volatility], axis=1)
+    comparison_data = comparison_data*100
+    comparison_data = comparison_data.T
+    comparison_data.index = ["Return", "Volatility"]
+
+    return comparison_data
+
+
+def global_minimum_variance(returns_df):
+
+    annualized_returns = annualized_rets(returns_df / 100, 365)
+    covariance = (returns_df / 100).cov()
+    weights = optimal_weights(25, annualized_returns, covariance)
+
+    rets = [portfolio_return(w, annualized_returns) for w in weights]
+    vols = [portfolio_vol(w, covariance) for w in weights]
+
+    ef = pd.DataFrame({"Returns": rets, "Volatility": vols})
+
+    w_gmv = gmv(covariance)
+    r_gmv = portfolio_return(w_gmv, annualized_returns)
+    vol_gmv = portfolio_vol(w_gmv, covariance)
+    w_gmv = pd.DataFrame([w_gmv*100], index=["Weights"], columns=returns_df.columns)
+
+    return [ef, w_gmv, r_gmv, vol_gmv]
--- ui.py
+++ ui.py
@@ -1,50 +1,58 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
-from endpoint import financial_metrics_calculation
+from endpoint import financial_metrics_calculation, financial_data_comparison, global_minimum_variance
+from utils import get_data
 import datetime
 
-time_measures = ['SEC', 'MIN', 'HRS', 'DAY', 'MTH', 'YRS']
-time_units = {
-    'SEC':['1', '2', '3', '4', '5', '6', '10', '15', '20', '30'],
-    'MIN': ['1', '2', '3', '4', '5', '6', '10', '15', '20', '30'],
-    'HRS': ['1', '2', '3', '4', '6', '8', '12'],
-    'DAY': ['1', '2', '3', '5', '7', '10'],
-    'MTH': ['1', '2', '3', '4', '6'],
-    'YRS': ['1', '2', '3', '4', '5']
-}
 
 @st.cache
-def fetch_data(coin1, coin2, time_unit, time_measure, init_time, final_time):
-    return financial_metrics_calculation(coin1, coin2, time_unit, time_measure, init_time.isoformat(), final_time.isoformat())
-
-with st.form("my_form"):
-    col1, col2 = st.columns(2)
-
-    with col1:
-        coin1 = st.text_input('Coin 1', value="BTC", key=1)
-        init_time = st.date_input("Beginning time of compparisson",  datetime.date(2022, 5, 1))
-        time_measure = st.selectbox('Time measure', time_measures, index=3)
-    with col2:
-        coin2 = st.text_input('Coin 2', value = 'ETH', key=2)
-        final_time = st.date_input("Final time of compparisson", datetime.date(2022, 8, 1))
-        time_unit = st.selectbox('Time unit', time_units[time_measure], index=0)
-    submitted = st.form_submit_button("Submit")
-
-if submitted:
-    data = fetch_data(coin1, coin2, time_unit, time_measure, init_time, final_time)
-    fig_return = px.line(data[0], title="Return of coins")
-    fig_return.update_layout(yaxis_title="Return")
-    fig_return['data'][0]['line']['color']="#332FD0"
-    fig_return['data'][1]['line']['color']="#D89216"
-    st.plotly_chart(fig_return)
-    fig_volatility = px.line(data[1], title="Volatility of coins")
-    fig_volatility.update_layout(yaxis_title="Volatility")
-    fig_volatility['data'][0]['line']['color']="#332FD0"
-    fig_volatility['data'][1]['line']['color']="#D89216"
-    st.plotly_chart(fig_volatility)
-    st.write(data[1])
+def fetch_data(coin1, coin2, init_time, final_time):
+    info_coin1 = get_data(coin1)
+    info_coin2 = get_data(coin2)
+    return financial_metrics_calculation(info_coin1, info_coin2, init_time, final_time)
 
 
+def create_graphs():
+
+    with st.form("my_form"):
+        col1, col2 = st.columns(2)
+
+        with col1:
+            coin1 = st.text_input('Asset 1', value="BTC", key=1)
+            init_time = st.date_input("Beginning time of comparison",  datetime.date(2022, 5, 1))
+        with col2:
+            coin2 = st.text_input('Asset 2', value='ETH', key=2)
+            final_time = st.date_input("Final time of comparison", datetime.date(2022, 8, 1))
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = fetch_data(coin1, coin2, init_time, final_time)
+        comparison_data = financial_data_comparison(data[0])
+        [ef, w_gmv, r_gmv, vol_gmv] = global_minimum_variance(data[0])
+        st.table(comparison_data)
+
+        fig_return = px.line(data[0], title="Assets Return")
+        fig_return.update_layout(yaxis_title="Return")
+        fig_return.update_layout(xaxis_title="Date")
+        fig_return['data'][0]['line']['color'] = "#332FD0"
+        fig_return['data'][1]['line']['color'] = "#D89216"
+        st.plotly_chart(fig_return)
+        fig_volatility = px.line(data[1], title="Assets Volatility")
+        fig_volatility.update_layout(yaxis_title="Volatility")
+        fig_volatility.update_layout(xaxis_title="Date")
+        fig_volatility['data'][0]['line']['color'] = "#332FD0"
+        fig_volatility['data'][1]['line']['color'] = "#D89216"
+        st.plotly_chart(fig_volatility)
+        fig_gmv = px.line(x=ef["Volatility"], y=ef["Returns"], title="Global Minimum Variance Optimization")
+        fig_gmv.add_scatter(x=[vol_gmv], y=[r_gmv], name="GMV Point")
+        fig_gmv.update_layout(yaxis_title="Return")
+        fig_gmv.update_layout(xaxis_title="Volatility")
+        st.plotly_chart(fig_gmv)
+        st.table(w_gmv)
+
+        # st.write(data[1])
 
 
+if __name__ == "__main__":
+
+    create_graphs()
